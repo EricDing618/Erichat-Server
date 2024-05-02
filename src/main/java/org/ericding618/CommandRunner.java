d8da66381c8d6c9be1018609ca82eb0771343824
Eric学编程 <1843794003@qq.com>
2024-05-02 13:58:31 +0800

    Changes

--- src/main/java/org/ericding618/CommandRunner.java
+++ src/main/java/org/ericding618/CommandRunner.java
@@ -1,4 +1,19 @@
 package org.ericding618;
 
+import java.util.*;
+import org.ericding618.JsonTool;
+import org.ericding618.tools;
+
 public class CommandRunner {
+    JsonTool CommandJson = new JsonTool();
+    tools tool = new tools();
+
+    public Boolean CommandNameIsTrue(String cmd) {
+        try {
+            Map Command = CommandJson.parseJSONToMap(tool.GetResourcePath("CommandConfig.json"));
+        }catch (Exception e){
+            System.out.println("错误：配置损坏。");
+            e.printStackTrace();
+        }
+    }
 }
