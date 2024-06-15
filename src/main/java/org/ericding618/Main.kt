b00899971afab063601e21b14c27446e037bbfac
Eric学编程 <1843794003@qq.com>
2024-06-15 15:31:44 +0800

    Changes

--- src/main/java/org/ericding618/Main.kt
+++ src/main/java/org/ericding618/Main.kt
@@ -1,5 +1,13 @@
 package org.ericding618
 
-fun main(args: Array<String>) {
-    println("hello world")
+fun main(args: Array<String>){
+    val version = 0.1
+    if (args.isEmpty()){
+        val tool = Tools()
+        tool.PrintTimeSleep("欢迎使用Erichat Server（v$version）！")
+        tool.PrintTimeSleep("当前时间为："+tool.GetTime()+" 。")
+        tool.PrintTimeSleep("检测：本程序正在"+tool.GetOS()+"操作系统上运行。")
+        tool.PrintTimeSleep("您可以输入“help”获取命令帮助，或者输入“quit”以退出程序。")
+        tool.PrintTimeSleep("您还可以输入其他命令，例如“login”或“create”。")
+    }
 }
